d15585c85dfd93f25606440b09932643dd61725b
last one <3dpr1nt3rb0y@gmail.com>
2024-12-17 18:32:17 -0600

    First commit

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='typeFX',
-    version='0.1.0',
-    description='A package with a typing effect and sound',
-    packages=find_packages(),
-    include_package_data=True,
+    name='typeFX',  # The name of your package
+    version='0.1.0',  # The version of your package
+    description='A package with a typing effect and sound',  # Short description
+    author='Diego Escalante',  # Add your name or your organization's name
+    author_email='3dpr1nt3rb0y@gmail.com',  # Add your email for contact
+    url='https://github.com/Blend3r/typeFX',  # URL to your GitHub repository
+    packages=find_packages(),  # Automatically find packages in your project
+    include_package_data=True,  # Include non-Python files specified in package_data
     package_data={
-        'typeFX': ['typeFX/sounds/spacebar-click-keyboard-199448-[AudioTrimmer.com].mp3'],  # Include sound files in the package
+        'typeFX': ['sounds/spacebar-click-keyboard-199448-[AudioTrimmer.com].mp3'],  # Make sure this path is correct
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',  # Python version
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',  # OS compatibility
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.13',  # Minimum required Python version
 )
